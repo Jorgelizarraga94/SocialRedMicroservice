c3c3d61c492d44a57800d05986466b5d939b2781
Jorgelizarraga94 <jorgelizarraga1994@gmail.com>
2026-06-24 19:24:01 -0300

    first commit

---
+++ Users/src/test/java/com/socials/Users/UsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.socials.Users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
