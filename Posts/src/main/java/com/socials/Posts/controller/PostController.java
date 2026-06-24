f24a5c4e7ebe278a8f1570e6ca32e517e38de6d5
Jorgelizarraga94 <jorgelizarraga1994@gmail.com>
2026-06-24 20:41:55 -0300

    fconfiguración principal de Post

--- Posts/src/main/java/com/socials/Posts/controller/PostController.java
+++ Posts/src/main/java/com/socials/Posts/controller/PostController.java
@@ -3,6 +3,7 @@ package com.socials.Posts.controller;
 import com.socials.Posts.model.Post;
 import com.socials.Posts.service.IPostService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,8 +17,12 @@ public class PostController {
     @Autowired
     private IPostService postService;
 
+    @Value("${server.port}")
+    public int serverPort;
+
     @GetMapping("/{userId}")
     public List<Post> getPostsByUserId(@PathVariable Long userId){
+        System.out.println("-----------Entrando en el puerto " + serverPort + " -----------" );
         return postService.getPostsByUser(userId);
     }
 }
